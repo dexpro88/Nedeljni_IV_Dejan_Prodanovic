85bf474ae1865a8bd8073d4654ca08882b5399bb
dexpro988 <dejan.prodanovic@panonit.com>
2020-09-01 02:26:00 -0700

    Dll script added

---
+++ Nedeljni_IV_Dejan_Prodanovic/DDLscript.sql
@@ -0,0 +1,66 @@
+--we create database  
+CREATE DATABASE MySocialNetwork;
+
+GO
+
+use MySocialNetwork;
+
+GO
+
+ --we delete tables in case they exist
+DROP TABLE IF EXISTS tblUser;
+DROP TABLE IF EXISTS tblPost;
+DROP TABLE IF EXISTS tblFriendRequests;
+DROP TABLE IF EXISTS tblFriends;
+DROP TABLE IF EXISTS tblLikedPosts;
+DROP TABLE IF EXISTS tblUserPosts;
+
+
+--we create table tblUser
+ CREATE TABLE tblUser (
+    UserID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    FirstName varchar(50),
+	LastName varchar(50),
+	Gender varchar(50),
+	Location varchar(50),
+    Email varchar(50),
+	Username varchar(50),
+	Password varchar(50)
+ 
+);
+--we create table tblSector 
+ CREATE TABLE tblPost (
+    PostID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	DateOfPost date,
+	PostText varchar(200),
+	NumberOfLikes int,
+	UserID int FOREIGN KEY REFERENCES tblUser(UserID)  
+);
+
+CREATE TABLE tblFriendRequests (
+   
+	SendUserId int FOREIGN KEY REFERENCES tblUser(UserID),
+	RecieveUserId int FOREIGN KEY REFERENCES tblUser(UserID)  ,
+	primary key(SendUserId,RecieveUserId)
+	);
+
+CREATE TABLE tblFriends (
+   
+	FirstUserId int FOREIGN KEY REFERENCES tblUser(UserID),
+	SecondUserId int FOREIGN KEY REFERENCES tblUser(UserID)  ,
+	primary key(FirstUserId,SecondUserId)
+	);
+
+CREATE TABLE tblLikedPosts (
+   
+	UserId int FOREIGN KEY REFERENCES tblUser(UserID),
+	PostId int FOREIGN KEY REFERENCES tblPost(PostID)  ,
+	primary key(UserId,PostId)
+	);
+
+CREATE TABLE tblUserPosts (
+   
+	UserId int FOREIGN KEY REFERENCES tblUser(UserID),
+	PostId int FOREIGN KEY REFERENCES tblPost(PostID)  ,
+	primary key(UserId,PostId)
+	);
---
+++ Nedeljni_IV_Dejan_Prodanovic/Nedeljni_IV_Dejan_Prodanovic/ViewModelBase/LoginViewModel.cs
@@ -0,0 +1,192 @@
+﻿using Nedeljni_IV_Dejan_Prodanovic.Command;
+using Nedeljni_IV_Dejan_Prodanovic.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Nedeljni_IV_Dejan_Prodanovic.ViewModelBase
+{
+    class LoginViewModel:ViewModelBase
+    {
+        LoginView view;
+        
+
+        public LoginViewModel(LoginView loginView)
+        {
+            view = loginView;
+           
+        }
+
+        private string userName;
+        public string UserName
+        {
+
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public string Error
+        {
+            get { return null; }
+        }
+
+        public string this[string someProperty]
+        {
+            get
+            {
+
+                return string.Empty;
+            }
+        }
+
+
+
+        private ICommand submitCommand;
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (submitCommand == null)
+                {
+                    submitCommand = new RelayCommand(Submit);
+                    return submitCommand;
+                }
+                return submitCommand;
+            }
+        }
+
+        void Submit(object obj)
+        {
+
+            string password = (obj as PasswordBox).Password;
+
+
+
+            if (string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Wrong user name or password");
+                return;
+            }
+            
+
+            //string encryptedString = EncryptionHelper.Encrypt(password);
+
+            //tblUser user = userService.GetUserByUserNameAndPassword(UserName, encryptedString);
+            //if (user != null)
+            //{
+            //    tblAdmin admin = adminService.GetAdminByUserId(user.UserID);
+
+            //    if (admin != null)
+            //    {
+            //        if (admin.AdministratorType.Equals("System"))
+            //        {
+            //            AdminMainView adminMainView = new AdminMainView(admin);
+            //            adminMainView.Show();
+            //            view.Close();
+            //            return;
+            //        }
+            //        else if (admin.AdministratorType.Equals("Local"))
+            //        {
+            //            LocaldAminMainView localAdminView = new LocaldAminMainView();
+            //            localAdminView.Show();
+            //            view.Close();
+            //            return;
+            //        }
+            //        else if (admin.AdministratorType.Equals("Team"))
+            //        {
+            //            TeamAdminView teamAdminView = new TeamAdminView();
+            //            teamAdminView.Show();
+            //            view.Close();
+            //            return;
+            //        }
+
+            //    }
+
+            //    tblManager manager = managerService.GetManagerByUserId(user.UserID);
+
+            //    if (manager != null)
+            //    {
+            //        if (string.IsNullOrEmpty(manager.ResponsibilityLevel))
+            //        {
+            //            string str1 = string.Format("You can not login\nLocal Admin has not gave you" +
+            //                " responsibility level yet");
+            //            MessageBox.Show(str1);
+            //            return;
+            //        }
+            //        ManagerMainView managerMainView = new ManagerMainView(manager);
+            //        managerMainView.Show();
+            //        view.Close();
+            //        return;
+            //    }
+            //    tblEmployee employee = employeeService.GetEmployeeByUserId(user.UserID);
+
+            //    if (employee != null)
+            //    {
+
+            //        EmployeeMainView employeeMainView = new EmployeeMainView(employee);
+            //        employeeMainView.Show();
+            //        view.Close();
+            //        return;
+            //    }
+
+            //    MessageBox.Show("Wrong username or password");
+            //}
+            
+
+            
+            //else
+            //{
+            //    MessageBox.Show("Wrong username or password");
+
+            //}
+
+
+        }
+
+        //private ICommand register;
+        //public ICommand Register
+        //{
+        //    get
+        //    {
+        //        if (register == null)
+        //        {
+        //            register = new RelayCommand(param => RegisterExecute(), param => CanRegisterExecute());
+        //        }
+        //        return register;
+        //    }
+        //}
+
+        //private void RegisterExecute()
+        //{
+        //    try
+        //    {
+        //        RegisterView register = new RegisterView();
+        //        register.Show();
+        //        view.Close();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.ToString());
+        //    }
+        //}
+        //private bool CanRegisterExecute()
+        //{
+
+        //    return true;
+        //}
+
+       
+    }
+}
